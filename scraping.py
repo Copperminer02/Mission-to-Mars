0b51f4943bcc9fd44fbfe29971393520eb0232a0
Big Papp J <copperminer02@gmail.com>
2022-02-19 16:00:25 -0700

    First Draft of Challenge Submission

--- scraping.py
+++ scraping.py
@@ -18,6 +18,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres":hemispheres(browser),
         "last_modified": dt.datetime.now()}
 
     # Stop webdriver and return data
@@ -96,7 +97,67 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
 
+# # D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles
+
+# ### Hemispheres
+def hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    # convert browser html to soup
+    html = browser.html
+    Hemi_soup = soup(html, 'html.parser')
+
+    # parse Hrefs for sites containing hemishphere info, save hrefs into 'href' list
+    link_soup= Hemi_soup.find('div',class_='collapsible results')
+    hrefs = []
+
+    for link in link_soup.find_all('a'):
+        if link.get('href') not in hrefs:
+            hrefs.append(link.get('href'))
+
+    # Loop through hrefs list and create f string urls by combining 'https://marshemispheres.com/' with href
+    # save results in url_list
+    url_list = []
+
+    for h in hrefs:
+        url_list.append(f'{url}{h}')
+
+    # loop through url_list to acces web pages for each hemishere    
+    for u in url_list:
+    # Create hemishpere dict and zero out title and image variable before each pass
+        hemispheres = {}
+        titles=0
+        images=0
+        
+    # visit each url in loop convert browser to html soup (m_soup)
+        browser.visit(u)   
+        html = browser.html
+        m_soup = soup(html, 'html.parser')
+
+    # Parse jpg href from each hemisphere url
+        step_1 = m_soup.find('div',class_='downloads')
+    # loop through all href instances and only choose href's with jpg 
+        for step in step_1.find_all('a'):
+            if 'jpg' in (step.get('href')):
+    # Save jpg href to images variable
+                images=step.get('href')
+    # assign dict values to keys img_url and title
+        hemispheres['img_url']=f'{url}{images}'
+        hemispheres['title']=m_soup.find('h2').text
+    # Append list of hemispheres dictionaries
+        hemisphere_image_urls.append(hemispheres)
+    #  end browser for next next url
+        browser.back()   
+   
+    return hemisphere_image_urls
+  
 if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
-
